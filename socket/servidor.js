95e9a50cfa5f46525304ef57ad954565ea9435fb
MJRUIZ1 <173193@ids.upchiapas.edu.mx>
2019-01-25 08:53:50 -0600

    socket

---
+++ socket/servidor.js
@@ -0,0 +1,36 @@
+var express = require ('express')
+var aplicacion = express()
+
+const net = require('net')
+const server = require ('http').Server(aplicacion)
+const sockest = require ('socket.io')(server)
+
+//var HOST = '192.168.43.138';
+var HOST = '192.168.1.73';
+var PORT = 3000 ;
+
+//server.listen(PORT, function() {
+//    console.log('Servidor Activo ' + PORT + ':' + HOST)
+//})
+
+var ser = net.createServer(function (so) {
+   // console.log(so)
+    console.log('Usuario Conectado:' + so.remoteAddress +':' + so.remotePort)
+    //so.on('connect', function () {
+     //   console.log('Usuario Nuevo')
+    //})
+
+    so.on('data', function(data) {
+     
+        //var dato = data.toString();
+        //console.log(data)
+       
+       // console.log( dato);
+    }) 
+
+    so.on('close', function () {
+        console.log('Usuario Desconectado')
+    })
+});
+
+ser.listen(PORT,HOST);
